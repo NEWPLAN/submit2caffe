6afb1ea0487c9693600df029327db95ca8e17fe9
NEWPLAN <enwplan001@163.com>
2018-09-14 17:18:04 +0800

    integrate

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include "hdparm.h"
 #include "hugepage.h"
 #include "glane_library.h"
+#include <string>
 
 int main(int argc, char const *argv[])
 {
+    string man_path = "/home/yang/project/imagenet/lmdb/ilsvrc12/train.txt";
+    string folder_path = "/mnt/dc_p3700/imagenet/train/";
     std::cout<<"Hello world..."<<std::endl;
     hugepage_main();
-    hdparm_main(argv[1],argv[2]);
+    glane_main();
+    hdparm_main(man_path.c_str(),folder_path.c_str());
     return 0;
 }
