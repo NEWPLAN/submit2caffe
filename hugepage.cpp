292bb002301703c50bce32fcb9743c5452b27769
NEWPLAN <enwplan001@163.com>
2018-09-17 09:01:30 +0800

    integrate

--- hugepage.cpp
+++ hugepage.cpp
@@ -9,19 +9,132 @@
 #include <errno.h>
 #include "hugepage.h"
 
-#define HUGEPAGE_FILE "/dev/hugepages1G/random"
-#define LENGTH (1024UL * 1024 * 1024)   // 1M
-#define PROTECTION (PROT_READ | PROT_WRITE)
-#define ADDR (void *)(0x0UL)
-#define FLAGS (MAP_SHARED | MAP_POPULATE)
-#define BAD_PHYS_ADDR 0
-#define PFN_MASK_SIZE 8
-
 static void write_byt(char *addr, char c);
 static void print_byt(char *addr);
 
 // Get physical address of any mapped virtual address in the current process
 static uint64_t mem_virt2phy(const void *virtaddr);
+
+#include <iostream>
+#include <string>
+class _hugepage_
+{
+  public:
+	explicit _hugepage_(std::string dev_info, uint32_t batch_size, uint32_t M_size);
+	~_hugepage_();
+
+	struct _batch_mem_
+	{
+		void *virt_addr = 0;
+		void *phy_addr = 0;
+		uint32_t size_in_m = 0;
+	}
+
+	protected : uint64_t
+				mem_virt2phy(const void *virtaddr);
+
+  private:
+	int MAX_LRNGTH = 1024 * 1024; //default is 1M
+
+	std::string dev_info;
+	int hugepage_fd = -1;
+
+	void *addr = nullptr;
+	uint64_t paddr = 0;
+
+	std::vector<_batch_mem_> batch_vect;
+
+} _hugepage_::_hugepage_(std::string dev_info, uint32_t batch_size, uint32_t M_size)
+{
+	this->MAX_LRNGTH = MAX_LRNGTH * M_size;
+	this->dev_info = dev_info;
+
+	if (dev_info.length() == 0 || batch_size == 0 || M_size == 0)
+	{
+		std::cerr << "error in init hugepage, out!" << std::endl;
+		exit(-1);
+	}
+
+	hugepage_fd = open(this->dev_info.c_str(), O_CREAT | O_RDWR, 0755);
+	if (hugepage_fd < 0)
+	{
+		std::cerr << "Fail to open hugepage memory file" << std::endl;
+		exit(-1);
+	}
+
+	addr = mmap(ADDR, LENGTH, PROTECTION, FLAGS, hugepage_fd, 0);
+	if (addr == MAP_FAILED)
+	{
+		std::cerr << "mmap error" << std::endl;
+		unlink(this->dev_info.c_str());
+		exit(-1);
+	}
+
+	paddr = this->mem_virt2phy(addr);
+}
+_hugepage_::_hugepage_()
+{
+	if (addr != nullptr)
+		munmap(addr, MAX_LRNGTH);
+	if (hugepage_fd >= 0)
+	{
+		close(hugepage_fd);
+		unlink(this->dev_info.c_str())
+	}
+}
+
+uint64_t _hugepage_::mem_virt2phy(const void *virtaddr)
+{
+	int fd, retval;
+	uint64_t page, physaddr;
+	unsigned long virt_pfn; // virtual page frame number
+	int page_size;
+	off_t offset;
+
+	/* standard page size */
+	page_size = getpagesize();
+	fd = open("/proc/self/pagemap", O_RDONLY);
+	if (fd < 0)
+	{
+		fprintf(stderr, "%s(): cannot open /proc/self/pagemap: %s\n", __func__, strerror(errno));
+		return BAD_PHYS_ADDR;
+	}
+	virt_pfn = (unsigned long)virtaddr / page_size;
+	//printf("Virtual page frame number is %llu\n", virt_pfn);
+
+	offset = sizeof(uint64_t) * virt_pfn;
+	if (lseek(fd, offset, SEEK_SET) == (off_t)-1)
+	{
+		fprintf(stderr, "%s(): seek error in /proc/self/pagemap: %s\n", __func__, strerror(errno));
+		close(fd);
+		return BAD_PHYS_ADDR;
+	}
+	retval = read(fd, &page, PFN_MASK_SIZE);
+	close(fd);
+	if (retval < 0)
+	{
+		fprintf(stderr, "%s(): cannot read /proc/self/pagemap: %s\n", __func__, strerror(errno));
+		return BAD_PHYS_ADDR;
+	}
+	else if (retval != PFN_MASK_SIZE)
+	{
+		fprintf(stderr, "%s(): read %d bytes from /proc/self/pagemap but expected %d:\n",
+				__func__, retval, PFN_MASK_SIZE);
+		return BAD_PHYS_ADDR;
+	}
+
+	/*
+	 * the pfn (page frame number) are bits 0-54 (see
+	 * pagemap.txt in linux Documentation)
+	 */
+	if ((page & 0x7fffffffffffffULL) == 0)
+	{
+		fprintf(stderr, "Zero page frame number\n");
+		return BAD_PHYS_ADDR;
+	}
+	physaddr = ((page & 0x7fffffffffffffULL) * page_size) + ((unsigned long)virtaddr % page_size);
+	return physaddr;
+}
 
 int hugepage_main(void)
 {
@@ -83,7 +196,7 @@ static uint64_t mem_virt2phy(const void *virtaddr)
 {
 	int fd, retval;
 	uint64_t page, physaddr;
-	unsigned long virt_pfn;	// virtual page frame number
+	unsigned long virt_pfn; // virtual page frame number
 	int page_size;
 	off_t offset;
 
@@ -101,7 +214,7 @@ static uint64_t mem_virt2phy(const void *virtaddr)
 	//printf("Virtual page frame number is %llu\n", virt_pfn);
 
 	offset = sizeof(uint64_t) * virt_pfn;
-	if (lseek(fd, offset, SEEK_SET) == (off_t) - 1)
+	if (lseek(fd, offset, SEEK_SET) == (off_t)-1)
 	{
 		fprintf(stderr, "%s(): seek error in /proc/self/pagemap: %s\n", __func__, strerror(errno));
 		close(fd);
@@ -118,7 +231,7 @@ static uint64_t mem_virt2phy(const void *virtaddr)
 	else if (retval != PFN_MASK_SIZE)
 	{
 		fprintf(stderr, "%s(): read %d bytes from /proc/self/pagemap but expected %d:\n",
-		        __func__, retval, PFN_MASK_SIZE);
+				__func__, retval, PFN_MASK_SIZE);
 		return BAD_PHYS_ADDR;
 	}
 
--- hugepage.h
+++ hugepage.h
@@ -1,11 +1,18 @@
-#ifndef __HUGE_PAGE__
-#define __HUGE_PAGE__
+#ifndef __MY_HUGE_PAGE__
+#define __MY_HUGE_PAGE__
 
 #if defined(__cplusplus)
 extern "C"
 {
 #endif
 
+#define HUGEPAGE_FILE "/dev/hugepages1G/random"
+#define LENGTH (1024UL * 1024 * 1024)   // 1M
+#define PROTECTION (PROT_READ | PROT_WRITE)
+#define ADDR (void *)(0x0UL)
+#define FLAGS (MAP_SHARED | MAP_POPULATE)
+#define BAD_PHYS_ADDR 0
+#define PFN_MASK_SIZE 8
 
 int hugepage_main(void);
 
